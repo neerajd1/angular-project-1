a41134be382e7195922fbd8cc1975c81d343f970
Neeraj Deochake <neerajdeochake12@gmail.com>
2020-09-24 16:42:35 +0530

    Isolated products

--- angular-project/src/app/layouts/products/products.component.ts
+++ angular-project/src/app/layouts/products/products.component.ts
@@ -8,8 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
-
   title = 'Products';
   mobParts = [
     {
@@ -17,18 +15,23 @@ export class ProductsComponent implements OnInit {
       prodName: 'Apple',
       prodInStock: 55,
       prodPrice: 80000,
+      country: 'Australia',
     },
     {
       prodId: 1002,
       prodName: 'Samsung',
       prodInStock: 105,
       prodPrice: 40000,
+      country: 'India',
     },
     {
       prodId: 1003,
       prodName: 'Apple',
       prodInStock: 0,
       prodPrice: 80000,
-    }
+      country: '',
+    },
   ];
+
+  ngOnInit(): void {}
 }
